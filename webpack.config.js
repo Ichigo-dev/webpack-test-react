a8dcf53e64687cc27fc920c2d256f89e4fd1c250
ichigo-dev <ichigo@web-freework.com>
2021-06-11 18:15:52 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/app.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "node_modules")],
+    extensions: [".js", ".ts", ".tsx"],
+  },
+    module: {
+    rules: [
+      {
+        test: [/\.ts$/, /\.tsx$/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+            },
+          },
+          'ts-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
+};
